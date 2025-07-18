043ded987c31bf86026d97008d7bd397a3e4154e
R_Sahariya <sahariyar98@gmail.com>
2025-07-19 02:39:42 +0530

    Result Uploader
    With all Dependency - For now I have used H2 database until what database we are storing it.

---
+++ resultUploader/src/main/java/com/qa/dashboard/ResultUploaderApplication.java
@@ -0,0 +1,13 @@
+package com.qa.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResultUploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResultUploaderApplication.class, args);
+	}
+
+}
---
+++ resultUploader/src/test/java/com/qa/dashboard/ResultUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResultUploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
